bbd780e78f696b8d452e8b0698d9f861f578e831
A6 <jake7625@outlookcom>
2024-09-11 17:43:10 -0400

    JSP example

---
+++ src/main/java/cs4050/A6/CinemaBookingSystem/CinemaBookingController.java
@@ -0,0 +1,14 @@
+package cs4050.A6.CinemaBookingSystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model; 
+
+@Controller
+public class CinemaBookingController {
+    
+    @RequestMapping("/")
+    public String home(Model map) {
+        return "homepage";
+    }
+}
