457d5c069bb52343bc20d6e671e654a5f961d70d
Jake <jake7625@outlook.com>
2024-09-11 16:10:26 -0400

    directory setup

---
+++ src/test/java/cs4050/A6/CinemaBookingSystem/CinemaBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cs4050.A6.CinemaBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
