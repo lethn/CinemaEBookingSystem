5247fd65ab60114721f37dcd8d26da6a38afd8bf
Jake <jake7625@outlook.com>
2024-09-18 15:19:40 -0400

    Order Confirmation started

---
+++ frontend/src/app/order-confirmation/page.jsx
@@ -0,0 +1,10 @@
+import NavBarAdmin from "../components/navBar";
+
+export default function OrderConfirmation() {
+    return (
+        <div>
+            <NavBar/>
+            Order Confirmation
+        </div>
+    );
+}
