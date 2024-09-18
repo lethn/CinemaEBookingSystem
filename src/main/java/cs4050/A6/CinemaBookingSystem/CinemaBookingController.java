a01d82ddc28209e00b3990d1f60c2e2b67f940ca
Jake <jake7625@outlook.com>
2024-09-18 16:03:07 -0400

    cleaned up old spring mvc files

--- src/main/java/cs4050/A6/CinemaBookingSystem/CinemaBookingController.java
+++ src/main/java/cs4050/A6/CinemaBookingSystem/CinemaBookingController.java
@@ -1,20 +1,8 @@
 package cs4050.A6.CinemaBookingSystem;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class CinemaBookingController {
-
-    @RequestMapping("/")
-    public String home(Model map) {
-        return "homepage";
-    }
-
-    @GetMapping("/login")
-    public String loginPage(Model map) {
-        return "login";
-    }
 }
