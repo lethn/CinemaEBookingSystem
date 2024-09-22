fd36b33ba43c99b748c9c6801efe9e4d1518bff6
Jake <jake7625@outlookcom>
2024-09-21 20:56:46 -0400

    order history initial

--- frontend/src/app/order-history/page.jsx
+++ frontend/src/app/order-history/page.jsx
@@ -10,6 +10,21 @@ export default function OrderHistory() {
         setUserRole(role);
     }, []);
 
+    // use api call to fetch order history for logged in user
+
+    const dummy_history = [
+        {
+            id: "1",
+            email: "example@gmail.com",
+            title: "Blade Runner 2049",
+            purchaseDate: "09/21/2024",
+            movieDate: "09/24/2024",
+            seats: ["A1", "A2"],
+            total: 50.00,
+        }
+    ];
+
+
     return (
         <div>
             <NavBar userRole={userRole} />
