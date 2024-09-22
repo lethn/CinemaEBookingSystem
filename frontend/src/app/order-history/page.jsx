de577d77409236bcd06a8553f73f8d978a6c6f9f
Jake <jake7625@outlookcom>
2024-09-21 21:29:20 -0400

    order history

--- frontend/src/app/order-history/page.jsx
+++ frontend/src/app/order-history/page.jsx
@@ -21,6 +21,24 @@ export default function OrderHistory() {
             movieDate: "09/24/2024",
             seats: ["A1", "A2"],
             total: 50.00,
+        },
+        {
+            id: "2",
+            email: "example@gmail.com",
+            title: "Interstellar",
+            purchaseDate: "09/21/2024",
+            movieDate: "09/25/2024",
+            seats: ["B1"],
+            total: 25.00,
+        },
+        {
+            id: "3",
+            email: "example@gmail.com",
+            title: "Morbius",
+            purchaseDate: "09/4/2024",
+            movieDate: "09/06/2024",
+            seats: ["C7"],
+            total: 25.00,
         }
     ];
 
@@ -28,7 +46,40 @@ export default function OrderHistory() {
     return (
         <div>
             <NavBar userRole={userRole} />
-            Order History
+            <div className="flex justify-center m-12 p-12">
+                <div className="bg-white p-12 m-auto shadow-lg rounded-lg">
+                    <table className="min-w-full border text-black">
+                        <thead>
+                            <tr>
+                                <th className="border p-2">Order ID</th>
+                                <th className="border p-2">Email</th>
+                                <th className="border p-2">Movie</th>
+                                <th className="border p-2">Purchase Date</th>
+                                <th className="border p-2">Showing Date</th>
+                                <th className="border p-2">Seats</th>
+                                <th className="border p-2">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dummy_history.map((order) => (
+                                <tr key={order.id}>
+                                    <td className="border p-2">{order.id}</td>
+                                    <td className="border p-2">{order.email}</td>
+                                    <td className="border p-2">{order.title}</td>
+                                    <td className="border p-2">{order.purchaseDate}</td>
+                                    <td className="border p-2">{order.movieDate}</td>
+                                    <td className="border p-2">{order.seats}</td>
+                                    <td className="border p-2">${order.total}</td>
+                                    {/* change to display dollar ammount later*/}
+                                </tr>
+                                
+                            
+                            
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
